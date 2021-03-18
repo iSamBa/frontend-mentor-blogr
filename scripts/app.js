a863cf652696571725e4378ba159176236f25075
iSamBa <bahou.aissam@gmail.com>
2021-03-18 21:37:56 +0100

    finalize menu items

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
 const burgerMenu = document.getElementsByClassName("burger-menu")[0];
 const closeMenu = document.getElementsByClassName("close-menu")[0];
+const menu = document.getElementsByClassName("menu")[0];
 
 burgerMenu.addEventListener("click", () => {
-    burgerMenu.classList.add("hidden")
-    closeMenu.classList.remove("hidden");
-
-})
+  burgerMenu.classList.add("hidden");
+  closeMenu.classList.remove("hidden");
+  menu.classList.remove("hidden");
+  console.log("HELLO");
+});
 
 closeMenu.addEventListener("click", () => {
-    closeMenu.classList.add("hidden");
-    burgerMenu.classList.remove("hidden")
-})
+  closeMenu.classList.add("hidden");
+  menu.classList.add("hidden");
+  burgerMenu.classList.remove("hidden");
+});
