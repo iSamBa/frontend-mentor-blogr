cd2fd78bb7fedcf7efbbcf6e9c612c32e2264e18
iSamBa <bahou.aissam@gmail.com>
2021-03-19 15:48:46 +0100

    add listeners to the whole iteme

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,19 @@ const menuItems = menu.getElementsByClassName("items")[0].getElementsByClassName
 
 Array.from(menuItems).forEach(item => {
   item.addEventListener("click", (e) => {
-    if (e.target.classList.contains("arrow-open")) {
-      e.target.classList.remove("arrow-open");
+
+    const arrowImg = e.target.getElementsByTagName("img")[0];
+    const itemContent = e.target.getElementsByTagName("ul")[0];
+    
+    console.log(arrowImg);
+    console.log(itemContent);
+    if (arrowImg.classList.contains("arrow-open")) {
+      arrowImg.classList.remove("arrow-open");
+      itemContent.classList.add("hidden");
     }
     else {
-      e.target.classList.add("arrow-open");
+      arrowImg.classList.add("arrow-open");
+      itemContent.classList.remove("hidden");
     }
   })
 });
