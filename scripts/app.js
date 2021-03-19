92fb880a347af758327947d048442f3cdaa6d38b
iSamBa <bahou.aissam@gmail.com>
2021-03-19 15:11:37 +0100

    add click event listener to arrow image

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,18 @@
 const burgerMenu = document.getElementsByClassName("burger-menu")[0];
 const closeMenu = document.getElementsByClassName("close-menu")[0];
 const menu = document.getElementsByClassName("menu")[0];
+const menuItems = menu.getElementsByClassName("items")[0].getElementsByTagName("img");
+
+Array.from(menuItems).forEach(item => {
+  item.addEventListener("click", (e) => {
+    e.target.classList.add("arrow-open")
+  })
+});
 
 burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.add("hidden");
   closeMenu.classList.remove("hidden");
   menu.classList.remove("hidden");
-  console.log("HELLO");
 });
 
 closeMenu.addEventListener("click", () => {
