c97e3aa08d09f996bbd47d4135f8d94d02a950b4
iSamBa <bahou.aissam@gmail.com>
2021-03-19 15:36:15 +0100

    change menu html structure

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,16 @@
 const burgerMenu = document.getElementsByClassName("burger-menu")[0];
 const closeMenu = document.getElementsByClassName("close-menu")[0];
 const menu = document.getElementsByClassName("menu")[0];
-const menuItems = menu.getElementsByClassName("items")[0].getElementsByTagName("img");
+const menuItems = menu.getElementsByClassName("items")[0].getElementsByClassName("item");
 
 Array.from(menuItems).forEach(item => {
   item.addEventListener("click", (e) => {
-    e.target.classList.add("arrow-open")
+    if (e.target.classList.contains("arrow-open")) {
+      e.target.classList.remove("arrow-open");
+    }
+    else {
+      e.target.classList.add("arrow-open");
+    }
   })
 });
 
