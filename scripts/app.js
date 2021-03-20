9e4ef788efaf49c0cf38019802b3db00280f2076
iSamBa <bahou.aissam@gmail.com>
2021-03-20 21:44:47 +0100

    finalize product menu item

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,25 @@
 const burgerMenu = document.getElementsByClassName("burger-menu")[0];
 const closeMenu = document.getElementsByClassName("close-menu")[0];
 const menu = document.getElementsByClassName("menu")[0];
-const menuItems = menu.getElementsByClassName("items")[0].getElementsByClassName("item");
+const menuItems = menu
+  .getElementsByClassName("items")[0]
+  .getElementsByClassName("item");
 
-Array.from(menuItems).forEach(item => {
+Array.from(menuItems).forEach((item) => {
   item.addEventListener("click", (e) => {
+    const arrowImg = e.target.parentElement.getElementsByTagName("img")[0];
+    const itemContent = e.target.parentElement.parentElement.getElementsByTagName(
+      "ul"
+    )[0];
 
-    const arrowImg = e.target.getElementsByTagName("img")[0];
-    const itemContent = e.target.getElementsByTagName("ul")[0];
-    
-    console.log(arrowImg);
-    console.log(itemContent);
     if (arrowImg.classList.contains("arrow-open")) {
       arrowImg.classList.remove("arrow-open");
       itemContent.classList.add("hidden");
-    }
-    else {
+    } else {
       arrowImg.classList.add("arrow-open");
       itemContent.classList.remove("hidden");
     }
-  })
+  });
 });
 
 burgerMenu.addEventListener("click", () => {
